a2817b90853c2117ceefaff58b9ecf6d047b19c9
Sebastian Helzle <sebastian@helzle.it>
2019-10-18 00:48:48 +0200

    FEATURE: Add basic module with empty view

---
+++ Classes/Controller/MediaController.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flowpack\Media\Ui\Controller;
+
+/*
+ * This file is part of the Flowpack.Media.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Mvc\View\JsonView;
+use Neos\Flow\Security\Context as SecurityContext;
+use Neos\Fusion\View\FusionView;
+use Neos\Neos\Controller\Module\AbstractModuleController;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class MediaController extends AbstractModuleController
+{
+    /**
+     * @var FusionView
+     */
+    protected $view;
+
+    /**
+     * @var string
+     */
+    protected $defaultViewObjectName = FusionView::class;
+
+    /**
+     * @var array
+     */
+    protected $supportedMediaTypes = ['application/json', 'text/html'];
+
+    /**
+     * @var array
+     */
+    protected $viewFormatToObjectNameMap = [
+        'html' => FusionView::class,
+        'json' => JsonView::class,
+    ];
+
+    /**
+     * @Flow\Inject
+     * @var SecurityContext
+     */
+    protected $securityContext;
+
+    public function indexAction()
+    {
+
+    }
+}
