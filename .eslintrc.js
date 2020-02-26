3be4176dabc92bc4e48af203d43d073920407b26
Sebastian Helzle <sebastian@helzle.it>
2020-02-26 11:06:32 +0100

    FEATURE: Prepare package for react development

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'react'
+  ],
+  /*parserOptions: {
+    jsx: true,
+    useJSXTextNode: true
+  },*/
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  ignorePatterns: ['.cache', 'dist', 'node_modules/'],
+  rules: {
+    '@typescript-eslint/indent': ['error', 4],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'prettier/prettier': ['error', {
+      semi: true,
+      singleQuote: true,
+      tabWidth: 4,
+      printWidth: 120
+    }]
+  }
+}
