51acd0c6d2a629005f3eae8d403fe5d7589c3429
Sebastian Helzle <sebastian@helzle.it>
2020-03-15 16:52:25 +0100

    TASK: Load ui theme from neos ui build essentials

--- Resources/Private/JavaScript/src/core/MediaUiTheme.tsx
+++ Resources/Private/JavaScript/src/core/MediaUiTheme.tsx
@@ -1,68 +1,13 @@
 import * as React from 'react';
 import { createTheming, createUseStyles } from 'react-jss';
 import { MediaUiTheme } from '../interfaces';
+import { config } from '@neos-project/build-essentials/src/styles/styleConstants';
 
 const ThemeContext = React.createContext({} as MediaUiTheme);
 const theming = createTheming(ThemeContext);
 const { ThemeProvider, useTheme } = theming;
 
-// Theme config from Neos.Ui package
-// TODO: Make css variables available in backend modules or include them differently
-const config = {
-    spacing: {
-        goldenUnit: '2.5rem',
-        full: '1rem',
-        half: '.5rem',
-        quarter: '.25rem'
-    },
-    size: {
-        sidebarWidth: '20rem'
-    },
-    transition: {
-        fast: '.1s',
-        default: '.25s',
-        slow: '.5s'
-    },
-    zIndex: {},
-    fontSize: {
-        base: '14px',
-        small: '12px'
-    },
-    fonts: {
-        headings: {
-            family: 'Noto Sans',
-            style: 'Regular',
-            cssWeight: '400'
-        },
-        copy: {
-            family: 'Noto Sans',
-            style: 'Regular',
-            cssWeight: '400'
-        }
-    },
-    colors: {
-        primaryViolet: '#26224C',
-        primaryVioletHover: '#342f5f',
-        primaryBlue: '#00ADEE',
-        primaryBlueHover: '#35c3f8',
-        contrastDarkest: '#141414',
-        contrastDarker: '#222',
-        contrastDark: '#3f3f3f',
-        contrastNeutral: '#323232',
-        contrastBright: '#999',
-        contrastBrighter: '#adadad',
-        contrastBrightest: '#FFF',
-        success: '#00a338',
-        successHover: '#0bb344',
-        warn: '#ff8700',
-        warnHover: '#fda23d',
-        error: '#ff460d',
-        errorHover: '#ff6a3c',
-        uncheckedCheckboxTick: '#5B5B5B'
-    }
-};
-
-// TODO: Directly use css variables or use above config as theme
+// Extend theme config from Neos.Ui package
 const mediaUiTheme: MediaUiTheme = {
     spacing: {
         ...config.spacing
