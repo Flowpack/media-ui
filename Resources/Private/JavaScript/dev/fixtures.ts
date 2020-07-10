5c41d5ca479377a25d71c28000a381a9c0d55ed4
Sebastian Helzle <sebastian@helzle.it>
2020-07-10 12:42:09 +0200

    TASK: Add assets to cloud asset source fixture for testing

--- Resources/Private/JavaScript/dev/fixtures.ts
+++ Resources/Private/JavaScript/dev/fixtures.ts
@@ -1,7 +1,25 @@
-import { Asset, AssetCollection, AssetSource, Tag } from '../src/interfaces';
+import { Asset, AssetCollection, AssetSource, IptcProperty, Tag } from '../src/interfaces';
+
+const exampleImages = ['example1.jpg', 'example2.jpg', 'example3.jpg'];
 
 const range = (length: number) => [...Array(length)].map((val, i) => i);
-const getExampleImage = (seed: number) => `Assets/example${(seed % 3) + 1}.jpg`;
+const getExampleFilename = (seed = 0) => exampleImages[seed % exampleImages.length];
+const getExampleImagePath = filename => `Assets/${filename}`;
+
+const getIptcProperties = (): IptcProperty[] => [
+    {
+        propertyName: 'Camera',
+        value: 'Phone'
+    },
+    {
+        propertyName: 'Exposure',
+        value: '3'
+    },
+    {
+        propertyName: 'SpecialSetting',
+        value: 'true'
+    }
+];
 
 const typeIcons = {
     jpg: {
@@ -23,7 +41,7 @@ const assetSources: AssetSource[] = [
         supportsCollections: true
     },
     {
-        id: 'example-source',
+        id: 'example-cloud-source',
         label: 'Example ☁️ Source',
         description: 'The source directly from the ☁️',
         iconUri: 'asset-source-icon.svg',
@@ -44,30 +62,35 @@ const assetCollections: AssetCollection[] = range(3).map(index => ({
     tags: range(index % 3).map(i => tags[(i * 3 + index) % tags.length])
 }));
 
-const assets: Asset[] = range(150).map(index => ({
-    id: index.toString(),
-    localId: index.toString(),
-    assetSource: assetSources[0],
-    imported: false,
-    label: `Example asset ${index + 1}`,
-    caption: `The caption for example asset ${index + 1}`,
-    filename: 'example1.jpg',
-    tags: range(index % 3).map(i => tags[(i * 3 + index) % tags.length]),
-    collections: range(index % 2).map(i => assetCollections[(i * 2 + index) % assetCollections.length]),
-    copyrightNotice: 'The Neos team',
-    lastModified: new Date('2020-06-16 15:07:00'),
-    iptcProperties: [],
-    width: 90,
-    height: 210,
-    file: {
-        extension: 'jpg',
-        mediaType: 'image/jpeg',
-        typeIcon: typeIcons.jpg,
-        size: 200,
-        url: getExampleImage(index)
-    },
-    thumbnailUrl: getExampleImage(index),
-    previewUrl: getExampleImage(index)
-}));
+const assets: Asset[] = range(150).map(index => {
+    const isCloud = index % 5 === 0;
+    const filename = getExampleFilename(index);
+
+    return {
+        id: index.toString(),
+        localId: index.toString(),
+        assetSource: assetSources[isCloud ? 1 : 0],
+        imported: isCloud && index % 3 === 0,
+        label: `Example asset ${index + 1}`,
+        caption: `The caption for example asset ${index + 1}`,
+        filename: 'example1.jpg',
+        tags: range(index % 3).map(i => tags[(i * 3 + index) % tags.length]),
+        collections: range(index % 2).map(i => assetCollections[(i * 2 + index) % assetCollections.length]),
+        copyrightNotice: 'The Neos team',
+        lastModified: new Date('2020-06-16 15:07:00'),
+        iptcProperties: index % 5 === 0 ? getIptcProperties() : [],
+        width: 90,
+        height: 210,
+        file: {
+            extension: 'jpg',
+            mediaType: 'image/jpeg',
+            typeIcon: typeIcons.jpg,
+            size: 200,
+            url: getExampleImagePath(filename)
+        },
+        thumbnailUrl: getExampleImagePath(filename),
+        previewUrl: getExampleImagePath(filename)
+    };
+});
 
 export { assetSources, assetCollections, tags, assets };
