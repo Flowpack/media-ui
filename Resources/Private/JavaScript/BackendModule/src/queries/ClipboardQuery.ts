4212c429e6fd7b9ce6155f9e2a52717749213f45
Sebastian Helzle <sebastian@helzle.it>
2021-04-08 17:52:43 +0200

    BUGFIX: Store correct assetsourceid in localstorage

--- Resources/Private/JavaScript/BackendModule/src/queries/ClipboardQuery.ts
+++ Resources/Private/JavaScript/BackendModule/src/queries/ClipboardQuery.ts
@@ -8,6 +8,6 @@ export const CLIPBOARD = gql`
 
 export const ADD_OR_REMOVE_FROM_CLIPBOARD = gql`
     mutation AddOrRemoveFromClipboard($assetId: AssetId, $assetSourceId: AssetSourceId) {
-        addOrRemoveFromClipboard(assetId: $assetId, assetSourceId: AssetSourceId) @client
+        addOrRemoveFromClipboard(assetId: $assetId, assetSourceId: $assetSourceId) @client
     }
 `;
