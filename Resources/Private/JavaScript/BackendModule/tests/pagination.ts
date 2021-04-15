44615ce5bbcfde1d932baee2063e330847cb5f3a
Sebastian Helzle <sebastian@helzle.it>
2021-04-15 14:16:06 +0200

    BUGFIX: Adjust test to pagination changes

--- Resources/Private/JavaScript/BackendModule/tests/pagination.ts
+++ Resources/Private/JavaScript/BackendModule/tests/pagination.ts
@@ -5,11 +5,8 @@ import { SERVER_NAME } from './helpers';
 fixture('Media Ui').page(SERVER_NAME);
 
 const firstThumbnail = Selector('[class^="thumbnail-"] [class^="caption-"]');
-const paginationPage2 = Selector('[class^="pagination-"] [class^="list-"] [class^="item-"]:nth-child(2)');
+const paginationPage2 = Selector('[class^="pagination-"] [class^="list-"] [class^="item-"]:nth-child(3)');
 
-test('Clicking next page shows more thumbnails', async t => {
-    await t
-        .click(paginationPage2)
-        .expect(firstThumbnail.innerText)
-        .eql('Example asset 21');
+test('Clicking next page shows more thumbnails', async (t) => {
+    await t.click(paginationPage2).expect(firstThumbnail.innerText).eql('Example asset 21');
 });
