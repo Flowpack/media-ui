6b08425c6a574da3eebe4ed78c9fc9bd936a6fde
Sebastian Helzle <sebastian@helzle.it>
2021-04-22 11:30:40 +0200

    TASK: Cleanup selection element

--- Resources/Private/JavaScript/BackendModule/src/components/SideBarRight/CurrentSelection.tsx
+++ Resources/Private/JavaScript/BackendModule/src/components/SideBarRight/CurrentSelection.tsx
@@ -1,19 +1,34 @@
 import * as React from 'react';
-import { useEffect, useMemo } from 'react';
-import { useRecoilState } from 'recoil';
+import { useMemo } from 'react';
+import { useRecoilValue } from 'recoil';
 import { fromString as getMediaTypeFromString } from 'media-type';
 
-import { Headline, SelectBox } from '@neos-project/react-ui-components';
+import { Headline, Icon } from '@neos-project/react-ui-components';
 
 import { useIntl, createUseMediaUiStyles, MediaUiTheme } from '@media-ui/core/src';
 import { useSelectedAsset, useSelectedAssetCollection, useSelectedTag } from '@media-ui/core/src/hooks';
 import { selectedInspectorViewState } from '@media-ui/core/src/state';
 
 const useStyles = createUseMediaUiStyles((theme: MediaUiTheme) => ({
-    currentSelection: {},
+    currentSelection: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
     headline: {
+        userSelect: 'none',
+        flex: '1 1 100%',
         fontWeight: 'bold',
         lineHeight: theme.spacing.goldenUnit,
+    },
+    label: {
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        '& svg': {
+            marginRight: theme.spacing.half,
+        },
     },
 }));
 
@@ -22,54 +37,45 @@ const CurrentSelection = () => {
     const selectedAsset = useSelectedAsset();
     const selectedAssetCollection = useSelectedAssetCollection();
     const selectedTag = useSelectedTag();
-    const [selectedInspectorView, setSelectedInspectorView] = useRecoilState(selectedInspectorViewState);
+    const selectedInspectorView = useRecoilValue(selectedInspectorViewState);
     const { translate } = useIntl();
 
-    const assetIcon = useMemo(() => {
-        if (selectedAsset?.file.mediaType) {
-            const mainMediaType = getMediaTypeFromString(selectedAsset.file.mediaType);
-            if (mainMediaType.type === 'audio') return 'file-audio';
-            if (mainMediaType.type === 'video') return 'file-video';
-            if (mainMediaType.type === 'image') return 'file-image';
-        }
-        return 'file';
-    }, [selectedAsset?.file.mediaType]);
+    const selection = useMemo(() => {
+        let icon = 'question';
+        let label = null;
 
-    // Create selection options for parent tag/collection
-    const options = [
-        selectedAssetCollection && {
-            value: 'assetCollection',
-            label: selectedAssetCollection.title,
-            icon: 'folder',
-        },
-        selectedTag && {
-            value: 'tag',
-            label: selectedTag.label,
-            icon: 'tag',
-        },
-        selectedAsset && { value: 'asset', label: selectedAsset.label, icon: assetIcon },
-    ].filter(Boolean);
-    const selectedOption = options.find((o) => o.value === selectedInspectorView);
-    const value = options.find((o) => o.value === selectedInspectorView) ? selectedInspectorView : options[0]?.value;
-
-    // @TODO get rid of this junk code in favour of something like this https://neos-project.slack.com/archives/CUEUD49ED/p1604002816009700
-    useEffect(() => {
-        if (!selectedOption) {
-            const firstValue = options[0]?.value;
-            if (firstValue) {
-                setSelectedInspectorView(firstValue as 'asset' | 'assetCollection' | 'tag');
+        if (selectedInspectorView === 'asset') {
+            if (selectedAsset?.file.mediaType) {
+                const mainMediaType = getMediaTypeFromString(selectedAsset.file.mediaType);
+                if (mainMediaType.type === 'audio') icon = 'file-audio';
+                if (mainMediaType.type === 'video') icon = 'file-video';
+                if (mainMediaType.type === 'image') icon = 'file-image';
+            } else {
+                icon = 'file';
             }
+            label = selectedAsset?.label;
+        } else if (selectedInspectorView === 'assetCollection') {
+            icon = 'folder';
+            label = selectedAssetCollection?.title;
+        } else if (selectedInspectorView === 'tag') {
+            icon = 'tag';
+            label = selectedTag?.label;
         }
-    });
 
-    if (!selectedAsset && !selectedAssetCollection) return null;
+        return { icon, label };
+    }, [selectedAsset, selectedTag, selectedAssetCollection, selectedInspectorView]);
+
+    if (!selection.label) return null;
 
     return (
         <div className={classes.currentSelection}>
             <Headline type="h2" className={classes.headline}>
                 {translate('currentSelection.headline', 'Selected element')}
             </Headline>
-            <SelectBox options={options} onValueChange={setSelectedInspectorView} value={value} />
+            <div className={classes.label} title={selection.label}>
+                <Icon icon={selection.icon} />
+                {selection.label}
+            </div>
         </div>
     );
 };
