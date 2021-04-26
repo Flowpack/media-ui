80aa7a0ae96ad1c2e16dad037185dc120e05f0fc
Sebastian Helzle <sebastian@helzle.it>
2021-04-26 09:49:30 +0200

    TASK: Small optimisation to main asset list

--- Resources/Private/JavaScript/BackendModule/src/components/Main/Main.tsx
+++ Resources/Private/JavaScript/BackendModule/src/components/Main/Main.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { useQuery } from '@apollo/client';
 
 import { Button } from '@neos-project/react-ui-components';
 
 import { useIntl, createUseMediaUiStyles, useMediaUi } from '@media-ui/core/src';
 import { clipboardState, useClipboard } from '@media-ui/feature-clipboard/src';
 
-import { VIEW_MODES } from '../../hooks';
+import { useViewModeSelection, VIEW_MODES } from '../../hooks';
 import { ListView, ThumbnailView } from './index';
-import { VIEW_MODE_SELECTION } from '../../queries';
 import LoadingLabel from '../LoadingLabel';
 
 const useStyles = createUseMediaUiStyles({
@@ -24,23 +22,23 @@ const useStyles = createUseMediaUiStyles({
 
 const Main: React.FC = () => {
     const classes = useStyles();
-    // TODO: Extract viewmode query into custom hook
-    const viewModeSelectionQuery = useQuery<{ viewModeSelection: VIEW_MODES }>(VIEW_MODE_SELECTION);
-    const { viewModeSelection } = viewModeSelectionQuery.data;
+    const [viewModeSelection] = useViewModeSelection();
     const { assets } = useMediaUi();
     const { clipboard } = useClipboard();
     const [{ visible: showClipboard }, setClipboardState] = useRecoilState(clipboardState);
     const { translate } = useIntl();
     const [assetIdentities, setAssetIdentities] = useState([]);
 
+    const queriedAssets = useMemo(() => {
+        return assets.map(({ id, assetSource }) => {
+            return { assetId: id, assetSourceId: assetSource.id };
+        });
+    }, [assets]);
+
     useEffect(() => {
-        const ids = showClipboard
-            ? clipboard
-            : assets.map(({ id, assetSource }) => {
-                  return { assetId: id, assetSourceId: assetSource.id };
-              });
-        window.setTimeout(() => setAssetIdentities(ids), 0);
-    }, [assets, clipboard, showClipboard, setAssetIdentities]);
+        const ids = showClipboard ? clipboard : queriedAssets;
+        setAssetIdentities(ids);
+    }, [queriedAssets, clipboard, showClipboard, setAssetIdentities]);
 
     return assetIdentities.length > 0 ? (
         viewModeSelection === VIEW_MODES.List ? (
