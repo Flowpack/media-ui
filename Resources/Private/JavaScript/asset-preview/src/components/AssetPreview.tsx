7e4a48944688e01e8e101c5a721e408ec67e53e3
Sebastian Helzle <sebastian@helzle.it>
2021-05-01 15:21:29 +0200

    FEATURE: Lightbox navigation and title & caption output

--- Resources/Private/JavaScript/asset-preview/src/components/AssetPreview.tsx
+++ Resources/Private/JavaScript/asset-preview/src/components/AssetPreview.tsx
@@ -7,6 +7,7 @@ import { createUseMediaUiStyles, useMediaUi, useMediaUiTheme } from '@media-ui/c
 
 import selectedAssetForPreviewState from '../state/selectedAssetForPreviewState';
 import { useAssetQuery } from '@media-ui/core/src/hooks';
+import { useMemo } from 'react';
 
 const useStyles = createUseMediaUiStyles({
     lightbox: {
@@ -19,9 +20,18 @@ const useStyles = createUseMediaUiStyles({
 export default function AssetPreview() {
     const classes = useStyles();
     const theme = useMediaUiTheme();
-    const { containerRef } = useMediaUi();
+    const { containerRef, assets } = useMediaUi();
     const [selectedAssetForPreview, setSelectedAssetForPreview] = useRecoilState(selectedAssetForPreviewState);
     const { asset } = useAssetQuery(selectedAssetForPreview);
+
+    const [prevAsset, nextAsset] = useMemo(() => {
+        if (!asset) return [null, null];
+        const currentAssetIndex = assets.findIndex(({ id }) => id === asset.id);
+        return [
+            currentAssetIndex > 0 ? assets[currentAssetIndex - 1] : null,
+            currentAssetIndex < assets.length - 1 ? assets[currentAssetIndex + 1] : null,
+        ];
+    }, [assets, asset]);
 
     // TODO: Handle pdf fiels with pdf viewer https://github.com/Flowpack/media-ui/issues/29
 
@@ -33,7 +43,26 @@ export default function AssetPreview() {
             reactModalProps={{ parentSelector: () => containerRef.current }}
             wrapperClassName={classes.lightbox}
             mainSrc={asset.previewUrl}
+            mainSrcThumbnail={asset.thumbnailUrl}
+            imageTitle={asset.label}
+            imageCaption={asset.caption}
             onCloseRequest={() => setSelectedAssetForPreview(null)}
+            prevSrc={prevAsset?.previewUrl}
+            prevSrcThumbnail={prevAsset?.thumbnailUrl}
+            prevLabel={prevAsset?.label}
+            onMovePrevRequest={() =>
+                setSelectedAssetForPreview(
+                    prevAsset ? { assetId: prevAsset.id, assetSourceId: prevAsset.assetSource.id } : null
+                )
+            }
+            nextSrc={nextAsset?.previewUrl}
+            nextSrcThumbnail={nextAsset?.thumbnailUrl}
+            nextLabel={nextAsset?.label}
+            onMoveNextRequest={() =>
+                setSelectedAssetForPreview(
+                    nextAsset ? { assetId: nextAsset.id, assetSourceId: nextAsset.assetSource.id } : null
+                )
+            }
         />
     );
 }
