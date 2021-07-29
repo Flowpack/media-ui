5c31687487e66aba89bd3daeaad63131dbffddcd
Sebastian Helzle <sebastian@helzle.it>
2021-07-29 11:21:12 +0200

    BUGFIX: Try to fix e2e tests

--- Resources/Private/JavaScript/media-module/tests/tags.ts
+++ Resources/Private/JavaScript/media-module/tests/tags.ts
@@ -4,19 +4,19 @@ import { SERVER_NAME } from './helpers';
 
 fixture('Media Ui').page(SERVER_NAME);
 
-const collectionTree = Selector('[class^="tree-"]');
-const firstThumbnail = Selector('[class^="thumbnail-"] [class^="caption-"]');
-const firstTag = collectionTree.find('[title="Example tag 1"]');
-const paginationPages = Selector('[class^="pagination-"] [class^="list-"] [class^="item-"]');
-const assetCount = Selector('[class^="assetCount-"]');
+const treeSelector = '[class^="tree-"]';
+const firstTagSelector = '[title="Example tag 1"]';
+const firstThumbnailSelector = '[class^="thumbnail-"] [class^="caption-"]';
+const paginationItemsSelector = '[class^="pagination-"] [class^="list-"] [class^="item-"]';
+const assetCountSelector = '[class^="assetCount-"]';
 
 test('Clicking first tag updates list and only assets should be shown that are assigned to it', async (t) => {
     await t
-        .click(firstTag)
-        .expect(firstThumbnail.innerText)
+        .click(Selector(treeSelector).find(firstTagSelector))
+        .expect(Selector(firstThumbnailSelector).innerText)
         .eql('Example asset 11')
-        .expect(paginationPages.count)
+        .expect(Selector(paginationItemsSelector).count)
         .eql(3) // one item and the two navigation buttons
-        .expect(assetCount.innerText)
+        .expect(Selector(assetCountSelector).innerText)
         .eql('12 assets');
 });
