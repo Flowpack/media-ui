2eadaa061c7f0858a6bd177a218e30da63922162
Daniel Lienert <daniel@lienert.cc>
2021-11-10 10:49:49 +0100

    FEATURE: Add first draft of tus upload backend

---
+++ Classes/Tus/TusEventHandler.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Flowpack\Media\Ui\Tus;
+
+/*
+ * This file is part of the Flowpack.Media.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\ResourceManagement\ResourceManager;
+use Neos\Media\Domain\Repository\AssetRepository;
+use Neos\Media\Domain\Service\AssetService;
+use Neos\Media\Domain\Strategy\AssetModelMappingStrategyInterface;
+use TusPhp\Events\TusEvent;
+
+class TusEventHandler
+{
+
+    /**
+     * @Flow\Inject
+     * @var ResourceManager
+     */
+    protected $resourceManager;
+
+    /**
+     * @Flow\Inject
+     * @var AssetModelMappingStrategyInterface
+     */
+    protected $assetModelMappingStrategy;
+
+    /**
+     * @Flow\Inject
+     * @var AssetService
+     */
+    protected $assetService;
+
+    /**
+     * @Flow\Inject
+     * @var AssetRepository
+     */
+    protected $assetRepository;
+
+    public function processUploadedFile(TusEvent $event): void
+    {
+        $persistentResource = $this->resourceManager->importResource($event->getFile()->getFilePath());
+        $targetType = $this->assetModelMappingStrategy->map($persistentResource);
+        $asset = new $targetType($persistentResource);
+        $this->assetService->getRepository($asset)->add($asset);
+    }
+}
