db2b16bc553ca6ea40530180516edc89a69e6560
Sebastian Helzle <sebastian@helzle.it>
2022-08-05 16:02:45 +0200

    BUGFIX: Prevent portrait images to exceed the thumbnail height in some cases

--- Resources/Private/JavaScript/media-module/src/components/Main/Thumbnail.tsx
+++ Resources/Private/JavaScript/media-module/src/components/Main/Thumbnail.tsx
@@ -53,6 +53,7 @@ const useStyles = createUseMediaUiStyles((theme: MediaUiTheme) => ({
                 'repeating-linear-gradient(45deg, #999999 25%, transparent 25%, transparent 75%, #999999 75%, #999999), repeating-linear-gradient(45deg, #999999 25%, #e5e5f7 25%, #e5e5f7 75%, #999999 75%, #999999)',
             backgroundPosition: '0 0, 10px 10px',
             backgroundSize: '20px 20px',
+            maxHeight: '250px',
         },
     },
     caption: {
