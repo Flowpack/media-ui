b5a87732e5dbfa3ca2440e57094741ba6225690d
Sebastian Helzle <sebastian@helzle.it>
2022-11-24 22:32:58 +0100

    BUGFIX: Make failing test run again on CI

--- Resources/Private/JavaScript/media-module/tests/assetCollections.ts
+++ Resources/Private/JavaScript/media-module/tests/assetCollections.ts
@@ -13,10 +13,10 @@ const assetCount = Selector('[class^="assetCount-"]');
 test('Clicking first collection updates list and only assets should be shown that are assigned to it', async (t) => {
     await t
         .click(firstTag)
-        .expect(firstThumbnail.innerText)
-        .eql('Example asset 4')
-        .expect(paginationPages.count)
-        .eql(3) // one item and the two navigation buttons
-        .expect(assetCount.innerText)
-        .eql('20 assets');
+        .expect(firstThumbnail.withText('Example asset 4').exists)
+        .ok('Asset 4 is not the first visible asset')
+        .expect(paginationPages.withText('3').exists)
+        .ok('Number of pages should be 3')
+        .expect(assetCount.withText('20 assets').exists)
+        .ok('Asset count does not show 20 items');
 });
--- Resources/Private/JavaScript/media-module/tests/tags.ts
+++ Resources/Private/JavaScript/media-module/tests/tags.ts
@@ -4,19 +4,19 @@ import { SERVER_NAME } from './helpers';
 
 fixture('Media Ui').page(SERVER_NAME);
 
-const treeSelector = '[class^="tree-"]';
+const treeSelector = Selector('[class^="tree-"]');
 const firstTagSelector = '[title="Example tag 1"]';
-const firstThumbnailSelector = '[class^="thumbnail-"] [class^="caption-"]';
-const paginationItemsSelector = '[class^="pagination-"] [class^="list-"] [class^="item-"]';
-const assetCountSelector = '[class^="assetCount-"]';
+const firstThumbnailSelector = Selector('[class^="thumbnail-"] [class^="caption-"]');
+const paginationItemsSelector = Selector('[class^="pagination-"] [class^="list-"] [class^="item-"]');
+const assetCountSelector = Selector('[class^="assetCount-"]');
 
 test('Clicking first tag updates list and only assets should be shown that are assigned to it', async (t) => {
     await t
-        .click(Selector(treeSelector).find(firstTagSelector))
-        .expect(Selector(firstThumbnailSelector).innerText)
-        .eql('Example asset 11')
-        .expect(Selector(paginationItemsSelector).count)
+        .click(treeSelector.find(firstTagSelector))
+        .expect(firstThumbnailSelector.withText('Example asset 11').exists)
+        .ok('The first thumbnail should have shown asset 11')
+        .expect(paginationItemsSelector.count)
         .eql(3) // one item and the two navigation buttons
-        .expect(Selector(assetCountSelector).innerText)
-        .eql('12 assets');
+        .expect(assetCountSelector.withText('12 assets').exists)
+        .ok('12 assets should have been shown');
 });
