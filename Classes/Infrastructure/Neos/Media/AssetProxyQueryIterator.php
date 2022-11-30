32d83143943384441e0810db2302c5b43b81a11c
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2022-11-30 13:27:27 +0100

    FEATURE: Enable search for assets by Id
    This includes the following changes:
    
    - A PHPUnit setup was added to the project
      - PHPUnit ^9.5
      - Unit tests can be found under `Tests/Unit`
    - This includes an additional job in the github actions `tests`
    workflow
    - The `SearchTerm` domain object has been introduced
    - A value object encapsulating newly introduced logic for search terms
    entered in the Media UI
    - A search term may now have a special format
    `/id:([0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12})/`
    in which case the search will look for an asset by its Id rather than
    performing a full text search
    - The `AssetProxyIteratorAggregate` domain object has been introduced
    - This has become necessary, because `QueryResolver` relies on
    processing iterables, which is fulfilled when dealing with an
    `AssetProxyQueryInterface` (or better:
    `AssetProxyQueryResultInterface`). However, looking up an asset by its
    Id via `AssetProxyRepositoryInterface::getAssetProxy` won't return an
    `AssetProxyQueryInterface`, but an `AssetProxyInterface`, which is not
    iterable.
      - There's two implementations of `AssetProxyIteratorAggregate`:
    - `AssetProxyListIterator` - will take 1 or more
    `AssetProxyInterface`s and provide the means to iterate over them
    - `AssetProxyQueryIterator` - will take an
    `AssetProxyQueryInterface` and resolves the query once iteration starts
    - The `createAssetProxyQuery` method of `QueryResolver` has been
    adjusted and is now called `createAssetProxyIterator`
    - Both, the `assetCount` and the `assets` method of `QueryResolver`
    have been adjusted accordingly

---
+++ Classes/Infrastructure/Neos/Media/AssetProxyQueryIterator.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flowpack\Media\Ui\Infrastructure\Neos\Media;
+
+/*
+ * This file is part of the Flowpack.Media.Ui package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\Media\Ui\Domain\Model\AssetProxyIteratorAggregate;
+use Neos\Media\Domain\Model\AssetSource\AssetProxy\AssetProxyInterface;
+use Neos\Media\Domain\Model\AssetSource\AssetProxyQueryInterface;
+
+/**
+ * @internal
+ */
+final class AssetProxyQueryIterator implements AssetProxyIteratorAggregate
+{
+    /**
+     * @var AssetProxyQueryInterface $assetProxyQuery
+     */
+    private $assetProxyQuery;
+
+    private function __construct(AssetProxyQueryInterface $assetProxyQuery)
+    {
+        $this->assetProxyQuery = $assetProxyQuery;
+    }
+
+    public static function from(AssetProxyQueryInterface $assetProxyQuery): self
+    {
+        return new self($assetProxyQuery);
+    }
+
+    public function setOffset(int $offset): void
+    {
+        try {
+            // TODO: Check if it's an issue to execute the query a second time just to get the correct number of results?
+            $offset = $offset < $this->assetProxyQuery->execute()->count() ? $offset : 0;
+        } catch (\Exception $e) {
+            // TODO: Handle that not every asset source implements the count method => Introduce countable interface?
+        }
+
+        $this->assetProxyQuery->setOffset($offset);
+    }
+
+    /**
+     * @param null|integer $limit
+     * @return void
+     */
+    public function setLimit($limit): void
+    {
+        // Unfortunately, AssetProxyQueryInterface::setLimit does not accept
+        // `null` as a value, so we must filter it first.
+        //
+        // TODO: This check can be removed, once the following issue has been solved:
+        // https://github.com/neos/neos-development-collection/issues/3962
+        if ($limit === null) {
+            throw new \Exception(
+                'Not supported: AssetProxyQueryInterface::setLimit does not accept `null`.',
+                1669221347
+            );
+        }
+
+        $this->assetProxyQuery->setLimit($limit);
+    }
+
+    public function count(): int
+    {
+        try {
+            return $this->assetProxyQuery->execute()->count();
+        } catch (\Exception $e) {
+            // TODO: Handle that not every asset source implements the count method => Introduce countable interface?
+            return 0;
+        }
+    }
+
+    /**
+     * @return \Traversable<AssetProxyInterface>
+     */
+    public function getIterator(): \Traversable
+    {
+        // TODO: It's not possible to use `toArray` here as not all asset sources implement it
+        yield from $this->assetProxyQuery->execute();
+    }
+}
