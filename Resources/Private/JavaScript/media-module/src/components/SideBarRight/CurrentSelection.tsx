e40b26f715b74543c3a8060de5d5ae42f15364a0
Sebastian Helzle <sebastian@helzle.it>
2023-01-23 14:56:15 +0100

    FEATURE: Only show current selection for tags and collections

--- Resources/Private/JavaScript/media-module/src/components/SideBarRight/CurrentSelection.tsx
+++ Resources/Private/JavaScript/media-module/src/components/SideBarRight/CurrentSelection.tsx
@@ -44,17 +44,7 @@ const CurrentSelection = () => {
         let icon = 'question';
         let label = null;
 
-        if (selectedInspectorView === 'asset') {
-            if (selectedAsset?.file.mediaType) {
-                const mainMediaType = getMediaTypeFromString(selectedAsset.file.mediaType);
-                if (mainMediaType.type === 'audio') icon = 'file-audio';
-                if (mainMediaType.type === 'video') icon = 'file-video';
-                if (mainMediaType.type === 'image') icon = 'file-image';
-            } else {
-                icon = 'file';
-            }
-            label = selectedAsset?.label;
-        } else if (selectedInspectorView === 'assetCollection') {
+        if (selectedInspectorView === 'assetCollection') {
             icon = 'folder';
             label = selectedAssetCollection?.title;
         } else if (selectedInspectorView === 'tag') {
@@ -65,12 +55,14 @@ const CurrentSelection = () => {
         return { icon, label };
     }, [selectedAsset, selectedTag, selectedAssetCollection, selectedInspectorView]);
 
-    if (!selection.label) return null;
+    if (!selection.label || selectedInspectorView === 'asset') return null;
 
     return (
         <div className={classes.currentSelection}>
             <Headline type="h2" className={classes.headline}>
-                {translate('currentSelection.headline', 'Selected element')}
+                {selectedInspectorView === 'assetCollection'
+                    ? translate('currentSelection.assetCollection.headline', 'Selected collection')
+                    : translate('currentSelection.tag.headline', 'Selected tag')}
             </Headline>
             <div className={classes.label} title={selection.label}>
                 <Icon icon={selection.icon} />
