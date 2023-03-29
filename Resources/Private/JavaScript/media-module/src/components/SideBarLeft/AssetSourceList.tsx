c0604c5f673e68bf382bb49d97b4734c4b7eb5b2
Sebastian Helzle <sebastian@helzle.it>
2023-03-29 10:28:11 +0700

    TASK: Improve asset source list styling

--- Resources/Private/JavaScript/media-module/src/components/SideBarLeft/AssetSourceList.tsx
+++ Resources/Private/JavaScript/media-module/src/components/SideBarLeft/AssetSourceList.tsx
@@ -1,39 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import cx from 'classnames';
 
 import { Headline } from '@neos-project/react-ui-components';
 
-import { useIntl, createUseMediaUiStyles, MediaUiTheme } from '@media-ui/core/src';
+import { useIntl } from '@media-ui/core';
 import { useAssetSourcesQuery, useSelectAssetSource } from '@media-ui/core/src/hooks';
 import { IconLabel } from '@media-ui/core/src/components';
-import { clipboardVisibleState } from '@media-ui/feature-clipboard/src';
+import { clipboardVisibleState } from '@media-ui/feature-clipboard';
 
-const useStyles = createUseMediaUiStyles((theme: MediaUiTheme) => ({
-    assetSourceList: {
-        border: `1px solid ${theme.colors.border}`,
-        padding: `0 ${theme.spacing.full}`,
-    },
-    item: {
-        display: 'flex',
-        alignItems: 'center',
-        '& a': {
-            fontWeight: 'normal',
-            cursor: 'pointer',
-            userSelect: 'none',
-            '&:hover': {
-                color: theme.colors.primary,
-            },
-        },
-    },
-    itemSelected: {
-        '&, .neos.neos-module &': {
-            color: theme.colors.primary,
-        },
-    },
-}));
+import classes from './AssetSourceList.module.css';
 
 const AssetSourceList: React.FC = () => {
-    const classes = useStyles();
     const { assetSources } = useAssetSourcesQuery();
     const { translate } = useIntl();
     const [selectedAssetSource, setSelectedAssetSource] = useSelectAssetSource();
@@ -52,23 +30,22 @@ const AssetSourceList: React.FC = () => {
 
     return (
         <nav className={classes.assetSourceList}>
-            <Headline type="h2">
+            <Headline type="h2" className={classes.header}>
                 <IconLabel icon="box" label={translate('assetSourceList.header', 'Media sources')} />
             </Headline>
             {assetSources?.map((assetSource) => (
-                <IconLabel
+                <button
                     key={assetSource.id}
-                    label={assetSource.label}
-                    iconUri={assetSource.iconUri}
-                    className={classes.item}
+                    type="button"
+                    className={cx(classes.item, selectedAssetSource?.id === assetSource.id && classes.itemSelected)}
+                    onClick={() => handleSelectAssetSource(assetSource.id)}
                 >
-                    <a
-                        className={selectedAssetSource?.id === assetSource.id ? classes.itemSelected : null}
-                        onClick={() => handleSelectAssetSource(assetSource.id)}
-                    >
-                        {assetSource.id === 'neos' ? translate('assetSource.local', 'Local') : assetSource.label}
-                    </a>
-                </IconLabel>
+                    <IconLabel
+                        label={assetSource.id === 'neos' ? translate('assetSource.local', 'Local') : assetSource.label}
+                        iconUri={assetSource.iconUri}
+                        className={classes.itemLabel}
+                    />
+                </button>
             ))}
         </nav>
     );
