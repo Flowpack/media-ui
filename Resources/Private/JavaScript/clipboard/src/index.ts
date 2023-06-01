c6089098390a836a3bb822d59b15837798d51423
Sebastian Helzle <sebastian@helzle.it>
2023-06-01 21:34:35 +0200

    TASK: Remove unused clipboard query

--- Resources/Private/JavaScript/clipboard/src/index.ts
+++ Resources/Private/JavaScript/clipboard/src/index.ts
@@ -1,6 +1,5 @@
 export { clipboardVisibleState } from './state/clipboardVisibleState';
 export { clipboardItemState, clipboardState } from './state/clipboardState';
-export { CLIPBOARD, TOGGLE_CLIPBOARD_STATE } from './queries/ClipboardQuery';
 export { default as ClipboardToggle } from './components/ClipboardToggle';
 export { default as ClipboardActions } from './components/ClipboardActions';
 export { default as ClipboardWatcher } from './components/ClipboardWatcher';
