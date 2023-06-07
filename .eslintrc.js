36960de4364e3863010cb8caed95109aaa969c08
Sebastian Helzle <sebastian@helzle.it>
2023-06-07 14:46:12 +0200

    BUGFIX: Only show warnings for TS interfaces that are assumed unused
    Actually they are used, but eslint is somehow confused.

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,9 +27,16 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                args: 'none', // FIXME: Eslint shows warnings for used interfaces
+            },
+        ],
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        'no-unused-vars': 'off',
         'prettier/prettier': ['error'],
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': [
