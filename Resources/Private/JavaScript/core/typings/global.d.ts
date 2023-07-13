1d6fcc43abced30c4cc007693acc2f0eb6b997b8
Sebastian Helzle <sebastian@helzle.it>
2023-07-13 17:01:22 +0200

    TASK: Remove unused upload endpoint

--- Resources/Private/JavaScript/core/typings/global.d.ts
+++ Resources/Private/JavaScript/core/typings/global.d.ts
@@ -46,5 +46,9 @@ type PaginationConfig = {
     maximumLinks: number;
 };
 
+type Endpoints = {
+    graphql: string;
+};
+
 type AssetType = 'image' | 'video' | 'audio' | 'document' | 'all';
 type MediaType = `${string}/${string}`;
