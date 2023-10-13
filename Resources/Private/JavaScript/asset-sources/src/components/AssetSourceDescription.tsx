4aaf84e008d127cb9ec5201ad24fe08f75718af6
Sebastian Helzle <sebastian@helzle.it>
2023-10-13 10:16:01 +0200

    FEATURE: Hide asset source description if only one is available
    Resolves: #208

--- Resources/Private/JavaScript/asset-sources/src/components/AssetSourceDescription.tsx
+++ Resources/Private/JavaScript/asset-sources/src/components/AssetSourceDescription.tsx
@@ -7,12 +7,14 @@ import { IconLabel } from '@media-ui/core/src/components';
 import { useSelectedAssetSource } from '../hooks/useSelectedAssetSource';
 
 import classes from './AssetSourceDescription.module.css';
+import { useAssetSourcesQuery } from '../hooks/useAssetSourcesQuery';
 
 const AssetSourceDescription: React.FC = () => {
     const { translate } = useIntl();
     const selectedAssetSource = useSelectedAssetSource();
+    const { assetSources } = useAssetSourcesQuery();
 
-    if (!selectedAssetSource?.description) return null;
+    if (!selectedAssetSource?.description || assetSources.length <= 1) return null;
 
     return (
         <ToggablePanel closesToBottom={true} className={classes.assetSourceDescription}>
