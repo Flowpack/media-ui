b626090a536607ab27328d9d5306d6742bfdeb1f
Markus Günther <info@unikka.de>
2024-10-28 10:06:00 +0100

    TASK: Localizes exception messages thrown by the package backend

--- Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
+++ Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
@@ -1,23 +1,24 @@
-import { onError } from '@apollo/client/link/error';
+import {onError} from '@apollo/client/link/error';
 
 const createErrorHandler = (notify: NeosNotification) => {
     const translate = (id, value = null, args = {}, packageKey = 'Flowpack.Media.Ui', source = 'Main') => {
         return window.NeosCMS.I18n.translate(id, value, packageKey, source, args);
     };
 
-    return onError(({ graphQLErrors, networkError }) => {
+    return onError(({graphQLErrors, networkError}) => {
         if (graphQLErrors) {
             graphQLErrors.map((data) => {
                 const isInternalError = data.extensions.category === 'internal';
-                let errorTitleLabel = isInternalError ? 'errors.internal.title' : 'errors.graphql.title';
+                const defaultErrorTitle = isInternalError ? translate('errors.internal.title', 'Internal server error') : translate('errors.graphql.title', 'Communication error');
                 let errorMessageLabel = '';
+                let errorTitleLabel = '';
                 if (data.extensions.errorCode) {
                     errorTitleLabel = `errors.${data.extensions.errorCode}.title`;
                     errorMessageLabel = `errors.${data.extensions.errorCode}.message`;
                 }
 
                 notify.error(
-                    translate(errorTitleLabel),
+                    translate(errorTitleLabel, defaultErrorTitle),
                     errorMessageLabel.length ? translate(errorMessageLabel) : data.message
                 );
             });
