edc10aed48f6f159e65ce8d3213ef9a614ef53a6
Markus Günther <info@unikka.de>
2024-10-28 15:13:26 +0100

    TASK: Resolve linting issues

--- Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
+++ Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
@@ -1,15 +1,17 @@
-import {onError} from '@apollo/client/link/error';
+import { onError } from '@apollo/client/link/error';
 
 const createErrorHandler = (notify: NeosNotification) => {
     const translate = (id, value = null, args = {}, packageKey = 'Flowpack.Media.Ui', source = 'Main') => {
         return window.NeosCMS.I18n.translate(id, value, packageKey, source, args);
     };
 
-    return onError(({graphQLErrors, networkError}) => {
+    return onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors) {
             graphQLErrors.map((data) => {
                 const isInternalError = data.extensions.category === 'internal';
-                const defaultErrorTitle = isInternalError ? translate('errors.internal.title', 'Internal server error') : translate('errors.graphql.title', 'Communication error');
+                const defaultErrorTitle = isInternalError
+                    ? translate('errors.internal.title', 'Internal server error')
+                    : translate('errors.graphql.title', 'Communication error');
                 let errorMessageLabel = '';
                 let errorTitleLabel = '';
                 if (data.extensions.errorCode) {
