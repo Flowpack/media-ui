77ad51dea9974e3f31f2715805b67564eb1c05a0
Markus Günther <info@unikka.de>
2024-11-04 14:11:28 +0100

    FEATURE: Use custom transformer for flow exceptions
    The t3n GraphQL package includes a transformer that bypasses Flow Framework exceptions in GraphQL error handling. The error currently lacks the exception code as part of the information. By including this code (often a timestamp), we can render custom error messages in the backend and support localization.

--- Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
+++ Resources/Private/JavaScript/media-module/src/core/CreateErrorHandler.ts
@@ -1,20 +1,24 @@
 import { onError } from '@apollo/client/link/error';
 
 const createErrorHandler = (notify: NeosNotification) => {
+    const translate = (id, value = null, args = {}, packageKey = 'Flowpack.Media.Ui', source = 'Main') => {
+        return window.NeosCMS.I18n.translate(id, value, packageKey, source, args);
+    };
+
     return onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors) {
             graphQLErrors.map((data) => {
-                const isInternalError = data.extensions.code === 'INTERNAL_SERVER_ERROR';
-
-                console.error(
-                    data.extensions.code === isInternalError ? '[Internal server error]' : '[GraphQL error]',
-                    data.path,
-                    data
-                );
+                const isInternalError = data.extensions.category === 'internal';
+                let errorTitleLabel = isInternalError ? 'errors.internal.title' : 'errors.graphql.title';
+                let errorMessageLabel = '';
+                if (data.extensions.errorCode) {
+                    errorTitleLabel = `errors.${data.extensions.errorCode}.title`;
+                    errorMessageLabel = `errors.${data.extensions.errorCode}.message`;
+                }
 
                 notify.error(
-                    data.extensions.code === isInternalError ? 'Internal server error' : 'Communication error',
-                    data.message
+                    translate(errorTitleLabel),
+                    errorMessageLabel.length ? translate(errorMessageLabel) : data.message
                 );
             });
         }
