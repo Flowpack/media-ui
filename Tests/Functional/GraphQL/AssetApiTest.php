39af639785de19b9bb70663dca7da62aaf808739
Bernhard Schmitt <schmitt@sitegeist.de>
2026-04-16 23:20:24 +0200

    Fix use statement

--- Tests/Functional/GraphQL/AssetApiTest.php
+++ Tests/Functional/GraphQL/AssetApiTest.php
@@ -21,9 +21,7 @@ use Flowpack\Media\Ui\Tests\Functional\AbstractMediaTestCase;
 use Flowpack\Media\Ui\Tests\Functional\TestAssetUsageStrategy;
 use Neos\Flow\Persistence\Doctrine\PersistenceManager;
 use Neos\Media\Domain\Repository\AssetRepository;
-
 use Neos\Media\Domain\Model\AssetInterface;
-use Neos\Media\Domain\Repository\AssetRepository;
 
 use function Wwwision\Types\instantiate;
 
