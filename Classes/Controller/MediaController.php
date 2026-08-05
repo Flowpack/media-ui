e6094bb70c79f8cb422b04a41d8d66b3d082d741
Michel Loew <michel.loew@jvm.ch>
2026-08-05 14:40:39 +0200

    FEATURE: Hide metadata-properties without ui-config

--- Classes/Controller/MediaController.php
+++ Classes/Controller/MediaController.php
@@ -144,6 +144,9 @@ class MediaController extends AbstractModuleController
 
         $config = [];
         foreach ($metaDataPropertyDefinitions as $propertyDefinition) {
+            if ($propertyDefinition->ui === null) {
+                continue;
+            }
             $propertyName = $propertyDefinition->name->value;
             $config[$propertyName] = [
                 'type' => $propertyDefinition->type->name,
